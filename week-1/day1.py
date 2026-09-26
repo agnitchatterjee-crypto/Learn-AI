ece563e7aedb48a36686a213c80a5584d02f4733
agnitchatterjee-crypto <agnit.chatterjee@gmail.com>
2026-09-27 01:16:19 +0530

    new changes

---
+++ week-1/day1.py
@@ -0,0 +1,101 @@
+# imports
+
+import os
+from dotenv import load_dotenv
+from fastapi import requests
+from scraper import fetch_website_contents, fetch_website_links
+from IPython.display import Markdown, display
+from openai import OpenAI
+
+
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+# Check the key
+
+if not api_key:
+    print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
+elif not api_key.startswith("sk-proj-"):
+    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key - see troubleshooting notebook")
+elif api_key.strip() != api_key:
+    print("An API key was found, but it looks like it might have space or tab characters at the start or end - please remove them - see troubleshooting notebook")
+else:
+    print("API key found and looks good so far!")
+
+
+message = "Hello, GPT! This is my first ever message to you! Hi!"
+
+messages = [{"role": "user", "content": message}]
+
+#print(messages)
+
+openai = OpenAI()
+
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant"},
+    {"role": "user", "content": "What is 2 + 2?"}
+]
+
+response = openai.chat.completions.create(model="gpt-4.1-nano", messages=messages)
+print(response.choices[0].message.content)
+
+# response = openai.chat.completions.create(model="gpt-5-nano", messages=messages)
+# response.choices[0].message.content
+
+
+#ed = fetch_website_contents("https://edwarddonner.com")
+
+ed = fetch_website_contents("https://edwarddonner.com")
+#print(ed)
+
+system_prompt = """
+You are a snarky assistant that analyzes the contents of a website,
+and provides a short, snarky, humorous summary, ignoring text that might be navigation related.
+Respond in markdown. Do not wrap the markdown in a code block - respond just with the markdown.
+"""
+
+# Define our user prompt
+
+user_prompt_prefix = """
+Here are the contents of a website.
+Provide a short summary of this website.
+If it includes news or announcements, then summarize these too.
+
+"""
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant"},
+    {"role": "user", "content": "What is 2 + 2?"}
+]
+
+response = openai.chat.completions.create(model="gpt-4.1-nano", messages=messages)
+response.choices[0].message.content
+
+
+# See how this function creates exactly the format above
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_prefix + website}
+    ]
+
+#print(messages_for(ed))
+
+
+def summarize(url):
+    website = fetch_website_contents(url)
+    response = openai.chat.completions.create(
+        model = "gpt-4.1-mini",
+        messages = messages_for(website)
+    )
+    print("Tokens consumed: " + str(response.usage.total_tokens))
+    return response.choices[0].message.content
+
+
+def display_summary(url):
+    summary = summarize(url)
+    print("Model response: " + summary)
+
+display_summary("https://edwarddonner.com")
